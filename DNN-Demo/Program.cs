0953deb6f491da8ecc56556bd864e6370883cb7a
Jaco Brink <jaco.brink@xero.com>
2022-06-05 08:09:12 +1200

    simulated 8 node

--- DNN-Demo/Program.cs
+++ DNN-Demo/Program.cs
@@ -1,45 +1,77 @@
 ﻿using DNN_Demo;
 
 
-// NOTE: Breaks once we go greater than 1..Why?
-int synapseMatrixColumns = 2;// 1;
-int synapseMatrixLines = 3;
+int synapseMatrixColumns = 1; // represents the columns of inputs
+int synapseMatrixLines = 8; // represents the neuron count
 var curNeuralNetwork = new NeuralNetWork(synapseMatrixColumns, synapseMatrixLines);
 
 Console.WriteLine("Synaptic weights before training:");
 PrintMatrix(curNeuralNetwork.SynapsesMatrix);
 
-// Original input
-//var trainingInputs = new double[,] { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 1 } };
-//var trainingOutputs = NeuralNetWork.MatrixTranspose(new double[,] { { 0, 1, 1, 0 } });
 
-var trainingInputs = new double[,] 
-{ 
-    { 0, 0, 1 },
-    { 0, 1, 1 },
-    { 1, 0, 1 },
-    { 0, 1, 0 },
-    { 1, 0, 0 },
-    { 1, 1, 1 },
-    { 0, 0, 0 }
+var trainingInputs = new double[,]
+{
+    { 0, 0, 0, 0, 1, 1, 1, 1 },
+    { 1, 1, 1, 1, 0, 0, 0, 0 }
 };
-var trainingOutputs = NeuralNetWork.MatrixTranspose(new double[,] { { 0, 1, 1, 1, 1, 0, 0 } });
+var trainingOutputs = NeuralNetWork.MatrixTranspose(new double[,] { { 1, 0 } });
 
-curNeuralNetwork.Train(trainingInputs, trainingOutputs, 10);
+curNeuralNetwork.Train(trainingInputs, trainingOutputs, 100);
 
 Console.WriteLine("\nSynaptic weights after training:");
 PrintMatrix(curNeuralNetwork.SynapsesMatrix);
 
 
 // testing neural networks against a new problem 
-var output = curNeuralNetwork.Think(new double[,] { { 1, 1, 0 } });
-Console.WriteLine("\nConsidering new problem [1, 1, 0] => :");
-PrintMatrix(output);
+//var output = curNeuralNetwork.Think(new double[,] { { 0, 1, 1 } });
+//Console.WriteLine("\nConsidering new problem [1, 0, 0] => :");
+
+var output = curNeuralNetwork.Think(new double[,] { { 0, 1, 1, 1, 0, 0, 0, 1 } });
+Console.WriteLine("\nConsidering new problem [0, 1, 1, 1, 0, 0, 0, 1] => :");
+PrintMatrix(output, true);
+
+output = curNeuralNetwork.Think(new double[,] { { 0, 0, 0, 1, 0, 1, 1, 1 } });
+Console.WriteLine("\nConsidering new problem [0, 0, 0, 1, 0, 1, 1, 1] => :");
+
+PrintMatrix(output, true);
+
+
 
 Console.Read();
 
 
-static void PrintMatrix(double[,] matrix)
+static void OriginalSample()
+{
+    // Original input
+    //var trainingInputs = new double[,] { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 1 } };
+    //var trainingOutputs = NeuralNetWork.MatrixTranspose(new double[,] { { 0, 1, 1, 0 } });
+}
+
+static void XORWithReverseSample()
+{
+    // Values 1 & 2 in the input matrix
+    // 0 & 0 = 0
+    // 0 & 1 = 1
+    // 1 & 0 = 1
+    // 1 & 1 = 1
+    // Value 3 = 1 -> value (1 &  2) reverse
+    // Value 3 = 0 -> value (1 & 2) unchanged
+
+    //var trainingInputs = new double[,] 
+    //{ 
+    //    { 0, 0, 1 },// 0 -> apply 1 = 1   
+    //    { 0, 0, 0 },// 0 -> apply 0 = 0
+    //    { 1, 0, 1 },// 1 -> apply 1 = 0
+    //    { 1, 0, 0 },// 1 -> apply 0 = 1
+    //    { 0, 1, 1 },// 1 -> apply 1 = 0
+    //    { 0, 1, 0 },// 1 -> apply 0 = 1
+    //    { 1, 1, 1 },// 1 -> apply 1 = 0
+    //    { 1, 1, 0 },// 1 -> apply 0 = 1
+    //};
+    //var trainingOutputs = NeuralNetWork.MatrixTranspose(new double[,] { { 1, 0, 0, 1, 0, 1, 0, 1 } });
+}
+
+static void PrintMatrix(double[,] matrix, bool includeRounded = false)
 {
     int rowLength = matrix.GetLength(0);
     int colLength = matrix.GetLength(1);
@@ -48,7 +80,12 @@ static void PrintMatrix(double[,] matrix)
     {
         for (int j = 0; j < colLength; j++)
         {
-            Console.Write(string.Format("{0} ", matrix[i, j]));
+            Console.Write($"raw: {matrix[i, j]}");
+            if (includeRounded)
+            {
+                var val = Math.Round(matrix[i, j]);
+                Console.WriteLine($" rounded: {val}");
+            }            
         }
         Console.Write(Environment.NewLine);
     }
---
+++ Library/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace Library
+{
+    // Neuron to Synases to Neuron
+    // NeuronA value (x) -> SynaseAa (w) -> Modify value (x * w) (z) -> NeuronC
+    // NeuronA value (x) -> SynaseAb (w) -> Modify value (x * w) (z) -> NeuronD
+    // NeuronB value (x) -> SynaseAc (w) -> Modify value (x * w) (z) -> NeuronC
+    // NeuronB value (x) -> SynaseAa (w) -> Modify value (x * w) (z) -> NeuronD
+
+    // List of (z) values recieved and processed in Neuron
+    // NeuronC sum inputs(NeuronAz + NeuronBz) (t) -> Process (t) in function f(t) -> Output to Synapses OR Output result
+    // NeuronD sum inputs(NeuronAz + NeuronBz) (t) -> Process (t) in function f(t) -> Output to Synapses OR Output result
+
+
+    public class Neuron
+    {
+        public List<object> Synases { get; private set; }
+
+        public Neuron()
+        {
+            Synases = new List<object>();
+        }
+    }
+
+    public class Synapse
+    {
+        public double Weight { get; private set; }
+
+    }
+}
